642fe2fff846ee2b9870d307d486d4210c8f79ea
Rodrigo <shaunkiplays@gmail.com>
2026-05-18 19:25:34 -0300

    feat: add middleware

---
+++ flashlearn-api/src/middlewares/authMiddleware.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+
+function authMiddleware(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: "Token nao informado" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.id;
+    req.userRole = decoded.role;
+    next();
+  } catch {
+    return res.status(401).json({ error: "Token invalido" });
+  }
+}
+
+module.exports = authMiddleware;
